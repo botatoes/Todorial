bc4ac1ddfcffee2f772a2f4c89a788467522d8fd
Bo <botatoes@gmail.com>
2018-12-18 01:58:20 -0800

    persist data with codeable and update to MVC

---
+++ Todorial/Controller/TodoListViewController.swift
@@ -0,0 +1,124 @@
+//
+//  ViewController.swift
+//  Todorial
+//
+//  Created by Bo-ying Fu on 12/15/18.
+//  Copyright © 2018 Botatoes. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+
+    var itemArray = [Item]()
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        let newItem = Item()
+        newItem.title = "Grocery"
+        itemArray.append(newItem)
+        
+        let newItem1 = Item()
+        newItem1.title = "Laundry"
+        itemArray.append(newItem1)
+        
+        let newItem2 = Item()
+        newItem2.title = "herpderp"
+        itemArray.append(newItem2)
+
+        loadItems()
+//        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        let item = itemArray[indexPath.row]
+        
+        //setting cell title and checkmark
+        cell.textLabel?.text = item.title
+        cell.accessoryType = item.done ? .checkmark : .none
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        saveItems()
+        
+        tableView.reloadData()
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+
+    }
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        let alert = UIAlertController(title: "Add new item", message: "", preferredStyle: .alert)
+        var textField = UITextField()
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+        }
+        
+        let action = UIAlertAction(title: "Add item", style: .default) { (action) in
+            //when user clicks add items
+            
+            let newItem = Item()
+            newItem.title = textField.text!
+            self.itemArray.append(newItem)
+            
+            self.saveItems()
+            
+            self.tableView.reloadData()
+        }
+        
+        let actionCancel = UIAlertAction(title: "Cancel", style: .default) { (action) in
+            //when user clicks add items
+            alert.dismiss(animated: true, completion: nil)
+        }
+        alert.addAction(action)
+        alert.addAction(actionCancel)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
+    func saveItems () {
+        let encoder = PropertyListEncoder()
+        
+        do {
+            let data = try encoder.encode(itemArray)
+            try data.write(to: dataFilePath!)
+        } catch {
+            print("Error encoding item array, \(error)")
+        }
+    }
+    
+    func loadItems () {
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            
+            do {
+                try itemArray = decoder.decode([Item].self, from: data)
+            } catch {
+                print("Error decoding item array, \(error)")
+
+            }
+        }
+        
+        
+        
+    }
+
+}
+
